e591d69eeab94267fd119d4d1014cca5a7b49d75
aleksejbigov@gmail.com <aleksejbigov@gmail.com>
2026-04-02 23:41:35 +0300

    fix: address

--- src/const/addresses.ts
+++ src/const/addresses.ts
@@ -9,6 +9,6 @@ export const addresses_mainnet = {
 };
 
 export const addresses_arbitrum_sepolia = {
-  staking: '0xE63Ad18e28E5f1BfD1Cf5F9B6Ed53fA8381dcBd4',
-  rewardsDistribution: '0x18629E223C19E2B332298Cd506BCAC06114C775d',
+  staking: '0x56F871F3AEA254Fc15a2B80BDbaEC7f75aC2fF73',
+  rewardsDistribution: '0x18629E223C10x7bb839000853BB9A3849BD3f8405D18085dd927a9E2B332298Cd506BCAC06114C775d',
 };
