365066459a8cbed8d0bf5d7e8c60fe700c8ef893
aleksejbigov <aleksejbigov@gmail.com>
2026-04-27 12:19:51 +0300

    fix: cta button hover

--- src/modules/k613-staking/k613Staking.styles.ts
+++ src/modules/k613-staking/k613Staking.styles.ts
@@ -424,9 +424,7 @@ export const CtaButton = styled(Button)(({ theme }) => ({
   color: 'rgba(0, 0, 0, 0.87)',
   boxShadow:
     '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
-  '&:hover': {
-    backgroundColor: theme.palette.primary.dark,
-  },
+
   '&.Mui-disabled': {
     backgroundColor: 'rgba(255, 255, 255, 0.12)',
     color: 'rgba(255, 255, 255, 0.3)',
