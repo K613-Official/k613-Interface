1f65c16c9510b21caeac422499ac359f5e163be1
aleksejbigov <aleksejbigov@gmail.com>
2026-08-15 06:45:48 +0300

    feat: uniswap pool link in footer

--- src/components/Footer/const.ts
+++ src/components/Footer/const.ts
@@ -1,24 +1,31 @@
 export const SOCIAL_LINKS = [
   {
     id: 0,
+    alt: 'uniswap',
+    // K613/USDC 0.05% pool on Monad
+    href: 'https://app.uniswap.org/explore/pools/monad/0xDD5557CEcFD7Ba0F5F2A1C38967d83Df2951a4F4',
+    icon: '/icons/uniswap.svg',
+  },
+  {
+    id: 1,
     alt: 'github',
     href: 'https://github.com/K613-Official',
     icon: '/icons/github.svg',
   },
   {
-    id: 1,
+    id: 2,
     alt: 'discord',
     href: 'https://discord.gg/4mVwFWkarC',
     icon: '/icons/discord.svg',
   },
   {
-    id: 2,
+    id: 3,
     alt: 'twitter-x',
     href: 'https://x.com/k613_official',
     icon: '/icons/twitter-x.svg',
   },
   {
-    id: 3,
+    id: 4,
     alt: 'gitbook',
     href: 'https://docs.k613.net/',
     icon: '/icons/gitbook.svg',
