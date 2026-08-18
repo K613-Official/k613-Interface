1ec93436643afac328bc91cb3ef9ea8b0c5aed90
aleksejbigov <aleksejbigov@gmail.com>
2026-08-18 14:52:48 +0300

    feat: apy

--- src/components/Footer/const.ts
+++ src/components/Footer/const.ts
@@ -1,15 +1,26 @@
 import { UNISWAP_K613_USDC_POOL_URL } from 'src/const/links';
 
-export const SOCIAL_LINKS = [
+type SocialLink = {
+  id: number;
+  alt: string;
+  href: string;
+  icon: string;
+  /** Optical size tweak: a solid round glyph reads heavier than an outline in the same box. */
+  scale?: number;
+};
+
+export const SOCIAL_LINKS: SocialLink[] = [
   {
     id: 0,
     alt: 'uniswap',
+    scale: 0.9,
     href: UNISWAP_K613_USDC_POOL_URL,
     icon: '/icons/uniswap.svg',
   },
   {
     id: 1,
     alt: 'telegram',
+    scale: 0.92,
     href: 'https://t.me/K613_Official',
     icon: '/icons/telegram.svg',
   },
@@ -28,12 +39,14 @@ export const SOCIAL_LINKS = [
   {
     id: 4,
     alt: 'twitter-x',
+    scale: 0.92,
     href: 'https://x.com/k613_official',
     icon: '/icons/twitter-x.svg',
   },
   {
     id: 5,
     alt: 'gitbook',
+    scale: 0.96,
     href: 'https://docs.k613.net/',
     icon: '/icons/gitbook.svg',
   },
